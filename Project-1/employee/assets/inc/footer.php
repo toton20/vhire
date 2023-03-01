a95aee082d3bd168fc61a17297747c9fd856008f
Javier <javierfloresparaohao2020@gmail.com>
2023-03-01 12:49:22 +0800

    Commit

---
+++ Project-1/employee/assets/inc/footer.php
@@ -0,0 +1,4 @@
+<div class="splash-footer">
+    &copy; 2023 - <?php echo date ('Y');?>
+    V-Hire Reservation System
+</div>
---
+++ Project-1/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>V-Hire Reservation System</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="includes/css/main.css" />
+		<noscript><link rel="stylesheet" href="includes/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.php">V-Hire Reservation System</a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="pass-login.php">Make Reservation</a></li>
+								<li><a href="employee/emp-login.php">Drivers Login</a></li>
+								<li><a href="admin/emp-login.php">Admin Login</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<div class="logo"></div>
+							<h2 class="text-warning">V-Hire Reservation System</h2>
+							<p>Book Now</a></p>
+						</div>
+					</section>
+
+					<section id="footer">
+						<div class="inner">
+							<h2 class="major">Contact us</h2>
+							
+							<ul class="contact">
+								<li class="icon solid fa-home">
+								V-Hire Reservation System<br />
+									
+								</li>
+								<li class="icon solid fa-phone">(+254) 127-0000</li>
+								<li class="icon solid fa-envelope"><a href="#">mail@guihulngan.com</a></li>
+								
+							<ul class="copyright">
+								<li>&copy; V-Hire Reservation System</li><li>Developed By: Student </li>
+							</ul>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="includes/js/jquery.min.js"></script>
+			<script src="includes/js/jquery.scrollex.min.js"></script>
+			<script src="includes/js/browser.min.js"></script>
+			<script src="includes/js/breakpoints.min.js"></script>
+			<script src="includes/js/util.js"></script>
+			<script src="includes/js/main.js"></script>
+
+	</body>
+</html>
